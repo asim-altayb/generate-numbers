c3076883c105fb5144876f40cfc3ad9359565065
asim altayb <asomy0909@gmail.com>
2018-08-03 00:10:04 +0200

    Create generate.java

---
+++ generate.java
@@ -0,0 +1,22 @@
+public class RandomNumber {
+public static void main(String[] args){
+
+generateMyNumber();
+
+//BY #ASIM_ALTAYB
+}
+public static void generateMyNumber()
+{
+String aNumber = "";
+for(int i=0 ; i<1000 ; i++){
+    aNumber+="096";//start with 
+    int n=0;
+n= (int)((Math.random() * 9000000)+1000000);//number of digits 8
+aNumber+=Integer.toString(n); start with + number of digits 8 = 10 digits 
+System.out.println((aNumber));
+aNumber="";// after all loop we need to be that is empty .
+    
+}
+}
+
+}
