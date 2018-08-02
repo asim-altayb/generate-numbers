f56929a0c2d3da1c3d50b1c5128eacd6e6b489f6
asim altayb <asomy0909@gmail.com>
2018-08-03 00:10:54 +0200

    Update generate.java

--- generate.java
+++ generate.java
@@ -11,8 +11,8 @@ String aNumber = "";
 for(int i=0 ; i<1000 ; i++){
     aNumber+="096";//start with 
     int n=0;
-n= (int)((Math.random() * 9000000)+1000000);//number of digits 8
-aNumber+=Integer.toString(n); start with + number of digits 8 = 10 digits 
+n= (int)((Math.random() * 9000000)+1000000);//number of digits 7
+aNumber+=Integer.toString(n); start with + number of digits 7 = 10 digits 
 System.out.println((aNumber));
 aNumber="";// after all loop we need to be that is empty .
     
